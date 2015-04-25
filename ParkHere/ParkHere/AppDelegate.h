6ee2c9cdd3b3d4c4ed6ba7ebd238d54a2effb16e
Alexandre Jacquemot <alexandre@utelly.com>
2015-04-25 10:17:29 +0100

    Start the repository

---
+++ ParkHere/ParkHere/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ParkHere
+//
+//  Created by Alexandre JACQUEMOT on 25/04/2015.
+//  Copyright (c) 2015 TransformiBeaconHackathon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ParkHere/ParkHere/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ParkHere
+//
+//  Created by Alexandre JACQUEMOT on 25/04/2015.
+//  Copyright (c) 2015 TransformiBeaconHackathon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
